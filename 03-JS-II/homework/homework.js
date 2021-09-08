1ef7ff161a9d4160507450518104e0aafdb79600
cristian-840 <martinezcristian840@gmail.com>
2021-09-08 11:08:36 -0300

    ejercicios 2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -136,10 +136,11 @@ function operadoresLogicos(num1, num2, num3) {
     return  "Número 1 es mayor y positivo" ;
   }
   else  if ( num3  >  num1  &&  num3  >  num2 )  {
-    return num3 + 1 ;
-
+    return  num3  +  1 ;
   }
-  return "false" ;
+  else  {
+    return false ;
+  }
 }
 
 function esPrimo(numero) {
@@ -148,24 +149,24 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if (  numero  <  2 ){
-    return "false" ;
+  if (numero < 2){
+    return false;
   }
-  if ( numero  ===  2 ){
-    return "true";
+  if (numero === 2){
+    return true;
   }
-  for ( var  i  =  2 ;  i  <  numero ;  i ++ )  {
-    if ( numero  %  i  ===  0 )  {
-      return "false" ;
+  for (i = 3; i < numero; i++){
+    if (numero % i === 0){
+      return false;
     }
   }
-  return "true" ;
+  return true;
 }
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-if (valor === "true"){
+if (valor === true){
   return "Soy verdadero";
 }
 return "Soy falso";
@@ -175,23 +176,38 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  let arrayTablaDel6 = []
-  for (let i = 0 ; i = 10 ; i++){
-    arrayTablaDel6.push(6*1)
+  var tabla = []
+  for (var a = 0; a < 11; a++){
+    tabla.push (a * 6)
   }
-  return arrayTablaDel6 ;
+  return tabla;
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  var cnum = 0;
+  var num = 'hola';
+  num = numero.toString(10);
+  cnum = num.length;
+  if(cnum === 3){
+    return true;
+  }
+  return false;
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var a = numero
+  var i = 0
+  do {
+    i = i + 1 ;
+    a = a + 5 ;
+  }
+  while (i < 8);
+  return a ;
 }
 
 
